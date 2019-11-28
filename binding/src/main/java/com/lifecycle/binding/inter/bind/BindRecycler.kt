a3c0f288a43224dbac73fae49582f5425c4483f4
Arvin.Wang <1033144294@qq.com>
2019-11-28 16:37:51 +0800

    modify inflate

--- binding/src/main/java/com/lifecycle/binding/inter/bind/BindRecycler.kt
+++ binding/src/main/java/com/lifecycle/binding/inter/bind/BindRecycler.kt
@@ -17,23 +17,24 @@ import com.lifecycle.binding.util.findLayoutView
  * Author: created by ArvinWang on 2019/11/15 10:28
  * Email: 1033144294@qq.com
  */
-interface BindRecycler<T, Binding : ViewDataBinding> : BindParse<T, Binding>, Inflate, Recycler{
+interface BindRecycler<T, Binding : ViewDataBinding> : BindParse<T, Binding>, Inflate, Recycler {
 
     override fun createView(context: Context, parent: ViewGroup, convertView: View?): View {
-        val b = convertView?.getTag(R.id.dataBinding)
         val layoutId = convertView?.getTag(R.id.inflate)?.let { (it as Inflate).layoutId() }
-        val binding = if(b is ViewDataBinding&&layoutId() == layoutId){
-            b.setVariable(Constant.parse,this)
-            b.setVariable(Constant.vm,t())
-            b.executePendingBindings()
-            b
-        }else parse(t(),context,parent,false)
+        val binding = convertView?.getTag(R.id.dataBinding).let {
+            if (it is ViewDataBinding && layoutId() == layoutId) {
+                it.setVariable(Constant.parse, this)
+                it.setVariable(Constant.vm, t())
+                it.executePendingBindings()
+                it
+            }else parse(t(),context,parent,false)
+        }
         val view = binding.root
-        view.setTag(R.id.inflate,this)
-        view.setTag(R.id.dataBinding,binding)
+        view.setTag(R.id.inflate, this)
+        view.setTag(R.id.dataBinding, binding)
         return view
     }
 
 
-    override fun layoutId()= findLayoutView(javaClass).layout[layoutIndex()]
+    override fun layoutId() = findLayoutView(javaClass).layout[layoutIndex()]
 }
